63662a5e31e19dfe0e5f27b86652ce0555c2f542
CY Chen <cyc@nvidia.com>
2024-04-18 19:16:53 -0700

    Improve randomization seed for generating node's random namespace
    Signed-off-by: CY Chen <cyc@nvidia.com>

--- isaac_ros_nitros/src/nitros_node.cpp
+++ isaac_ros_nitros/src/nitros_node.cpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
-// Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2022-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ const std::vector<std::string> PRESET_EXTENSION_SPEC_NAMES = {
 namespace
 {
 // Generate a random string of the given length
-unsigned int seed = time(NULL);
+unsigned int seed = time(NULL) ^ getpid();
 std::string GenerateRandomString(const size_t length)
 {
   std::string output_string;
